8672dbe501a2be1c9a7951ccd2781c64c8f6eb00
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2023-05-04 23:53:49 +0200

    feat: improve tabs

--- www/components/UI/organisms/title-bar.tsx
+++ www/components/UI/organisms/title-bar.tsx
@@ -8,8 +8,13 @@ export default function TitleBar({ children }: { children: React.ReactNode }) {
       data-tauri-drag-region
       className={tw`bg-backgroundAlt flex w-full select-none overflow-hidden`}
     >
-      <div className={tw`flex px-2 items-center h-full border(border 0 b-1 t-0) pointer-events-none select-none`}>
-        <img src="/public/32x32.png" className={tw`flex h-5 w-5 object-contain `} />
+      <div
+        className={tw`flex px-2 items-center h-full border(border 0 b-2 t-0) pointer-events-none select-none`}
+      >
+        <img
+          src="/public/32x32.png"
+          className={tw`flex h-5 w-5 object-contain `}
+        />
       </div>
       {children}
       <WindowControls />
